eb36b0d52593d1c79b100ab83e06a11087f5fcbc
Wagner Rodrigo da Silva <wagnerrodrigo.pan@gmail.com>
2019-04-25 15:17:54 -0300

    simples calendario

---
+++ inicio/calendario.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    <h1><?php echo " titulo dentro do h1 com php"?></h1>
+    <table border="1">
+        <tr>
+            <th>Dom</th>
+            <th>Seg</th>
+            <th>Ter</th>
+            <th>Qua</th>
+            <th>Qui</th>
+            <th>Sex</th>
+            <th>Sab</th>
+        </tr>
+    </table>
+</body>
+</html>
