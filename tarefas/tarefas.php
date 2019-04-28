3dec2e4fe4978766e205d04e68d804706d3fe467
Wagner Rodrigo da Silva <wagnerrodrigo.pan@gmail.com>
2019-04-28 18:34:15 -0300

    tela de tarefa salvando apenas um item por vez

---
+++ tarefas/tarefas.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Gerenciador de tarefas</title>
+</head>
+
+<body>
+    <h1>Gerenciador de tarefas</h1>
+    <form>
+        <fieldset>
+            <legend>nova Tarefa</legend>
+            <label>
+                Tarefa:
+                <input type="text" name="nome">
+            </label>
+            <input type="submit" value="Cadastrar">
+        </fieldset>
+    </form>
+
+
+
+
+    <?php
+    $lista_tarefas = array();
+
+    if (isset($_GET['nome'])) {
+        $lista_tarefas[] = $_GET['nome'];
+    }
+    ?>
+
+    <table>
+        <tr>
+            <th>Tarefas</th>
+        </tr>
+        <?php foreach ($lista_tarefas as $tarefa) : ?>
+            <tr>
+                <td>
+                    <?php echo $tarefa; ?>
+                </td>
+            </tr>
+        <?php endforeach; ?>
+
+    </table>
+
+</body>
+
+</html>
